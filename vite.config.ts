f67166374c3dbbb9c88cd4141cba4411b7435c5c
Mardeni Mihardi <mardenimihardi@gmail.com>
2025-02-19 05:27:10 +0700

    tes

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_'); // Hanya baca variabel yang diawali VITE_
+
   return {
     plugins: [react()],
     build: {
       outDir: "dist",
-    }
+    },
+    define: {
+      'import.meta.env': JSON.stringify(env), // Paksa variabel masuk ke import.meta.env
+    },
   }
 })
